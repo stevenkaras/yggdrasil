78e69d6836e56ecdff791f478144fe11743d8927
Steven Karas <steven.karas@gmail.com>
2012-12-22 21:42:28 +0200

    add success message to common unit tests

--- test/ygg/common/unit_tests.cpp
+++ test/ygg/common/unit_tests.cpp
@@ -6,7 +6,10 @@
 #include "ygg/common/logging/logging_tests.h"
 #include "ygg/common/threading/lock_tests.h"
 
+#include <stdio.h>
+
 void unit_tests() {
     LoggingTests_TestSuite();
     LockTests_TestSuite();
+    printf("All common tests passed!");
 }
