5e3c16be9a520c2425000bb1428184ee599da6f5
Steven Karas <steven.karas@gmail.com>
2013-01-03 14:51:04 +0200

    program name feature stub

---
+++ src/ygg/common/system/program.h
@@ -0,0 +1,19 @@
+//
+// See doc/LICENSE.md for license terms
+//
+
+#ifndef YGG_COMMON_SYSTEM_PROGRAM_H_INCLUDED
+#define YGG_COMMON_SYSTEM_PROGRAM_H_INCLUDED
+
+namespace ygg {
+namespace system {
+
+/**
+ * Get the path to the executable for this program
+ */
+const char * get_executable_path();
+
+} /* namespace ygg::system */
+}
+
+#endif /* YGG_COMMON_SYSTEM_PROGRAM_H_INCLUDED */
